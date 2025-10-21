98862286957a6a7b582476c55628b42b8f1230c3
launathrodriguez <lauranrodriguezb@gmail.com>
2025-10-20 19:08:18 -0500

    Created improved version with animations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+    en: "English",
+    es: "Español",
+  };
+  
+  export const defaultLang = "en";
+  
+  export const ui = {
+    en: {
+        "menu.home": "Home",
+        "menu.about": "About",
+        "menu.experience": "Experience",
+        "menu.projects": "Projects",
+        "hero.title": "Hi, my name is",
+        "hero.description": "I'm Frontend Developer from Colombia",
+        "about.description": "Front-end Developer passionate about building responsive and accessible web applications. I combine modern stacks and frameworks with a strong business understanding to transform objectives into measurable and impactful digital experiences. My technical experience allows me to efficiently bridge the gap between design and development, ensuring seamless collaboration and successful product delivery. I am driven by continuous learning and adapting to new technologies to deliver innovative and high-quality solutions.",
+    },
+    es: {
+        "menu.home": "Home",
+        "menu.about": "Acerca de",
+        "menu.experience": "Experiencia",
+        "menu.projects": "Proyectos",
+        "hero.title": "Hola, mi nombre es",
+        "hero.description": "Soy Desarrolladora Frontend de Colombia",
+        "about.description": "Desarrolladora Frontend apasionada por construir aplicaciones web responsivas. Combino tecnologías y frameworks modernos con una sólida comprensión del negocio para transformar objetivos en experiencias digitales medibles e impactantes. Mi experiencia técnica me permite cerrar eficientemente la brecha entre diseño y desarrollo, asegurando una colaboración fluida y una entrega exitosa del producto. Me motiva el aprendizaje continuo y la adaptación a nuevas tecnologías para ofrecer soluciones innovadoras y de alta calidad.",
+    },
+  } as const;
